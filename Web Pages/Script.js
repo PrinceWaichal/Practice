24525bcd964791b1d90287c256e075b3d7777073
Prince Waichal <princeironman@gmail.com>
2018-08-21 12:03:03 +0530

    Changes in CSS & JS

--- Web Pages/Script.js
+++ Web Pages/Script.js
@@ -1,1 +1,23 @@
 // JavaScript source code
+// Create a "close" button and append it to each list item
+var List = document.getElementsByTagName( "li" );
+var i;
+for ( i = 0; i < List.length; i++ )
+{
+    var span = document.createElement( "Span" );
+    var text = document.createTextNode( "\u00D7" );
+    span.className = "Close";
+    span.appendChild( text );
+    List[i].appendChild( span );
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByName( "close" );
+var i;
+for ( i = 0; i < close.length; i++ )
+{
+    close[i].onclick=function()
+    {
+        var div = this.par
+    }
+}
